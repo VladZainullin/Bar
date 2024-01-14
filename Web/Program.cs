daec2000f0b3e326857a5e1a7569a11db4013531
Vladislav Zainullin <mittoface02@mail.ru>
2024-01-14 19:41:17 +0300

    Изменяет конфигурацию program

--- Web/Program.cs
+++ Web/Program.cs
@@ -2,10 +2,11 @@ using Application;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services
     .AddPersistenceServices(builder.Configuration)
     .AddApplicationServices();
+
+var app = builder.Build();
 
 app.Run();
