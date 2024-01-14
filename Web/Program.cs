e215adbaafe2e476486ec94d93efa68f596327a3
Vladislav Zainullin <mittoface02@mail.ru>
2024-01-14 19:38:06 +0300

    Добавляет конфигурацию зависимостей для сборки Application

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,9 +1,11 @@
+using Application;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 builder.Services
-    .AddPersistenceServices(builder.Configuration);
+    .AddPersistenceServices(builder.Configuration)
+    .AddApplicationServices();
 
 app.Run();
